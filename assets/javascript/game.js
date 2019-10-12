27eca247a05f5c8c5d6b7b1261e522e90a4bae04
Paola L <paolalarrazabal@gmail.com>
2019-10-12 06:36:42 -0400

    added if and else statement for each click event, under the if statement when numbers math added win++ and linked to html, same for else if statement except with lose conditions, added the total of the gems with each click, then added the startOver function to reset the values of the gems and the random number variable.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,108 @@
 $(document).ready(function() {
 
+// assigned variables //
 
 let randomNumber = Math.floor((Math.random() * 120) + 19);
 document.getElementById("Random-Number").innerHTML = randomNumber;
 
-let pinkGem = 0;
-let blueGem = 0;
-let redGem = 0;
-let yellowGem = 0;
+
 let wins = 0;
 let losses = 0;
 
-pinkGem = Math.floor((Math.random() * 12) + 1);
-blueGem = Math.floor((Math.random() * 12) + 1);
-redGem = Math.floor((Math.random() * 12) + 1);
-yellowGem = Math.floor((Math.random() * 12) + 1);
+let pinkGem = Math.floor((Math.random() * 12) + 1);
+let blueGem = Math.floor((Math.random() * 12) + 1);
+let redGem = Math.floor((Math.random() * 12) + 1);
+let yellowGem = Math.floor((Math.random() * 12) + 1);
 
 
+let GemTotal = 0;
+console.log(GemTotal);
 
 
+// resets the game when player wins/losses // 
+
+function startOver () {
+	randomNumber = Math.floor((Math.random() * 120) + 19);
+	document.getElementById("Random-Number").innerHTML = randomNumber;
+	pinkGem = Math.floor((Math.random() * 12) + 1);
+	blueGem = Math.floor((Math.random() * 12) + 1);
+	redGem = Math.floor((Math.random() * 12) + 1);
+	yellowGem = Math.floor((Math.random() * 12) + 1);
+	GemTotal = 0;
+	$('#gemTotal').text('Your Total Score is: ' + GemTotal);
+
+
+}
+
+// click events for each crystal // 
 
 $('#Pink-Gem').on('click', function() {
 console.log(pinkGem);
+GemTotal = pinkGem + GemTotal;
+$('#gemTotal').text('Your Total Score is: ' + GemTotal);
+if (GemTotal === randomNumber) {
+	wins ++;
+	$('#wins').text('Wins: ' + wins);
+	startOver();
+}
 
-
+else if (GemTotal > randomNumber) {
+	losses ++;
+	$('#losses').text('Losses: '+ losses);
+  startOver();
+}
 });
 
 $('#Blue-Gem').on('click', function() {
 console.log(blueGem);
+GemTotal = blueGem + GemTotal;
+$('#gemTotal').text('Your Total Score is: ' + GemTotal);
 
+if (GemTotal === randomNumber) {
+	wins ++;
+	$('#wins').text('Wins: ' + wins);
+	startOver();
+}
+
+else if (GemTotal > randomNumber) {
+	losses ++;
+	$('#losses').text('Losses: '+ losses);
+	startOver();
+}
 });
 
 $('#Red-Gem').on('click', function() {
 console.log(redGem);
+GemTotal = redGem + GemTotal;
+$('#gemTotal').text('Your Total Score is: ' + GemTotal);
+if (GemTotal === randomNumber) {
+	wins ++;
+	$('#wins').text('Wins: ' + wins);
+	startOver();
+}
 
+else if (GemTotal > randomNumber) {
+	losses ++;
+	$('#losses').text('Losses: '+ losses);
+	startOver();
+}
 });
 
 $('#Yellow-Gem').on('click', function() {
 console.log(yellowGem);
+GemTotal = yellowGem + GemTotal;
+$('#gemTotal').text('Your Total Score is: ' + GemTotal);
+if (GemTotal === randomNumber) {
+	wins ++;
+	$('#wins').text('Wins: ' + wins);
+	startOver();
+}
 
+else if (GemTotal > randomNumber) {
+	losses ++;
+	$('#losses').text('Losses: '+ losses);
+	startOver();
+}
 });
 
 
